c620507617a99c0a7d87861405e1cf9b90a68a7c
bryanagamk <bryan@kyodo-i.com>
2022-04-13 06:14:49 +0700

    fix bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,22 +31,23 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillContent = document.getElementsByClassName('skills__content'),
-    skillHeader = document.querySelectorAll('.skills__header')
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
 
 function toggleSkills() {
     let itemClass = this.parentNode.className;
 
-    for (i = 0; i < skillContent.length; i++) {
-        skillContent[i].className = 'skills__content skills__close'
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
     }
 
     if (itemClass === 'skills__content skills__close') {
-        this.parentNode.className = 'skillss__content skills__open'
+        this.parentNode.className = 'skills__content skills__open'
     }
 }
 
-skillHeader.forEach((el) => {
+skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills);
 })
 
